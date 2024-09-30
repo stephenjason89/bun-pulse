45b157345e66bd2edc1fc64dc61f2b11b2fdd143
Stephen Jason Wang <stephenjasonwang@gmail.com>
2024-09-30 15:01:02 +0800

    feat(utils): add utilities for generating socket IDs, HMAC SHA-256 hex digests, & consola message logger

---
+++ src/utils.ts
@@ -0,0 +1,22 @@
+import { createHmac } from 'node:crypto'
+import { createConsola } from 'consola'
+
+// Simple utility to generate unique socket ID
+export function generateSocketId() {
+	return `${Math.floor(Date.now() % 1e9)}.${Math.floor(Math.random() * 1e9)}`
+}
+
+// Generates a HMAC SHA-256 hex digest for the given message and secret.
+export function generateHmacSHA256HexDigest(message: string, secret: string): string {
+	const hmac = createHmac('sha256', secret)
+	hmac.update(message)
+	return hmac.digest('hex')
+}
+
+export const messageLogger = createConsola({
+	fancy: true,
+	formatOptions: {
+		colors: true,
+		compact: false,
+	},
+})
